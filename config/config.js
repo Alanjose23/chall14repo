7d5b7e84b744d3fdc35337548cd3bbed5ce2ed62
ALAN JOSE <alanjose04231120@gmail.com>
2022-11-23 23:38:32 -0500

    heroku2

--- config/config.js
+++ config/config.js
@@ -3,33 +3,12 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our db
-// const sequelize = process.env.JAWSDB_URL
-//   ? new Sequelize(process.env.JAWSDB_URL)
-//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       port: 3306
-//     });
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
 
-
-const Sequelize = require('sequelize');
-sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
-  }
-);
-
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
-
-  module.exports = sequelize;
+module.exports = sequelize;
