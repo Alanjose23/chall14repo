22f98400f0db1bceaf0ec66731504245d8299d92
ALAN JOSE <alanjose04231120@gmail.com>
2022-11-23 21:03:40 -0500

    created models as per standard

--- models/Comment.js
+++ models/Comment.js
@@ -2,16 +2,42 @@ const { Sequelize, Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/config');
 
 class Comment extends Model {}
-
+// created models based on user model
 Comment.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
     body: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+    },
+    // refernce the user id
+    userid: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    // reference the post as primary id key
+    postid: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "comment",
+        key: "id"
+      }
     }
   },
   {
-    sequelize
+    sequelize,
+    modelName: "comment",
+    timestamps: true,
+    underscored: true,
+    freezeTableName: true
   }
 );
 
--- models/Post.js
+++ models/Post.js
@@ -2,14 +2,37 @@ const { Sequelize, Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/config');
 
 class Post extends Model {}
-
+// created models based on user model
 Post.init(
   {
-    title: DataTypes.STRING,
-    body: DataTypes.STRING
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    body: {
+      type: DataTypes.INTEGER,
+    },
+    userid: {
+      type:DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      }
+    }
+    
   },
   {
-    sequelize
+    sequelize,
+    underscored: true,
+    freezeTableName: true,
+    modelName: "post",
+    timestamps: false
   }
 );
 
