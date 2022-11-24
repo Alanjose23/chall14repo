ceb1d95d42c41afe57e189fd5daeafdb1ea93af0
ALAN JOSE <alanjose04231120@gmail.com>
2022-11-23 23:20:47 -0500

    server message

--- config/config.js
+++ config/config.js
@@ -1,22 +1,14 @@
 const Sequelize = require('sequelize');
+
 require('dotenv').config();
 
-let sequelize;
-
-if (process.env.HEROKU_POSTGRESQL_RED_URL) {
-  sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_RED_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: '127.0.0.1',
       dialect: 'mysql',
-      port: 3306||process.env.port
-    }
-  );
-}
+      port: 3306 || process.env.PORT
+    });
 
 module.exports = sequelize;
-
